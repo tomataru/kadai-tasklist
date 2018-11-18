a4d124166094a9863d03a7f03bc1eb4517be4148
tom.ataru <ataru.tomita@gmail.com>
2018-11-18 08:40:55 +0000

    kadai-add-status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content,:status)
   end
 
 
--- app/models/message.rb
+++
@@ -1,3 +0,0 @@
-class Message < ApplicationRecord
-    validates :content, presence: true, length: { maximum: 255 }
-end
